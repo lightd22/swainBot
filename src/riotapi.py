9af6d9b00ca18db73b90cbf76fe9ed2bb1db868e
Devin Light <naavi09@gmail.com>
2016-10-30 22:22:55 -0700

    Starting interface with Riot's API

---
+++ src/riotapi.py
@@ -0,0 +1,8 @@
+def setApiKey(key):
+    """
+    Set user's API Key
+
+    Args:
+        key (string): the Riot API key desired for use.
+    """
+    return key = "71ab791f-d5fe-45b3-8b3a-0368ce261cbe"
